70bdb8cc0e2e890158fcaee63e1102e9f10e4dbb
zbkm <zubokome@gmail.com>
2025-11-02 15:02:20 +0500

    chore: move cache to var/cache*; add phpunit config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,10 +1,12 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->exclude(["var"]);
 
 return (new PhpCsFixer\Config())
+    ->setCacheFile(__DIR__ . "/var/cache/.php-cs-fixer.cache")
     ->setRules([
-        '@PSR12' => true
+        "@PSR12" => true
     ])
     ->setFinder($finder);
